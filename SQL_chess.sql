ff473228791349d19c3d588ff5d1900681d90de5
Victoria <viky.fomina@yandex.ru>
2020-06-08 22:00:15 +0300

    making procedure

--- SQL_chess.sql
+++ SQL_chess.sql
@@ -106,7 +106,8 @@ SELECT color, COUNT(color) AS count FROM chessman INNER JOIN chessboard ON chess
 SELECT type_of FROM chessman INNER JOIN chessboard ON chessman.id = chessboard.id_chessman WHERE color = 'black';
 
 -- 8) Какие фигуры черных имеются на доске? Вывести тип и количество. 
-SELECT type_of, COUNT(type_of) AS count FROM chessman INNER JOIN chessboard ON chessman.id = chessboard.id_chessman WHERE color = 'black' GROUP BY type_of;
+SELECT type_of, COUNT(type_of) AS count FROM chessman INNER JOIN chessboard ON chessman.id = chessboard.id_chessman 
+        WHERE color = 'black' GROUP BY type_of;
 
 -- 9) Найдите типы фигур (любого цвета), которых осталось, по крайней мере, не меньше двух на доске. 
 SELECT type_of FROM chessman INNER JOIN chessboard ON chessman.id = chessboard.id_chessman GROUP BY type_of HAVING COUNT(type_of) >= 2;
@@ -117,13 +118,13 @@ SELECT TOP(1) WITH TIES color FROM chessman INNER JOIN chessboard ON chessman.id
 -- 11) Найдите фигуры, которые стоят на возможном пути движения ладьи (rock) (Любой ладьи любого цвета). (Ладья может двигаться по горизонтали или 
 -- по вертикали относительно своего положения на доске в любом направлении.). 
 
-SELECT boardRestFigures.id_chessman FROM chessman figures, chessboard boardRook, chessboard boardRestFigures WHERE
-        figures.id = 1 AND boardRook.id_chessman = 1 AND boardRestFigures.id_chessman != 1 and 
+SELECT boardRestFigures.id_chessman FROM chessman figures, chessboard boardRook, chessboard boardRestFigures 
+        WHERE figures.id = 1 AND boardRook.id_chessman = 1 AND boardRestFigures.id_chessman != 1 AND 
         ((boardRook.x - boardRestFigures.x = 0) OR (boardRook.y - boardRestFigures.y = 0));
 
 -- 12) У каких игроков (цвета) еще остались ВСЕ пешки (pawn)? 
-SELECT color FROM chessman INNER JOIN chessboard ON (chessman.id = chessboard.id_chessman AND type_of = 'pawn') GROUP BY color
-        HAVING COUNT(color) = 8;
+SELECT color FROM chessman INNER JOIN chessboard ON (chessman.id = chessboard.id_chessman AND type_of = 'pawn') 
+        GROUP BY color HAVING COUNT(color) = 8;
         
 -- 13) Пусть отношения board1 и board2 представляют собой два последовательных состояние игры (Chessboard). Какие фигуры (cid) изменили свою 
 -- позицию (за один ход это может быть передвигаемая фигура и возможно еще фигура, которая была “съедена”)?
@@ -134,31 +135,45 @@ SELECT color FROM chessman INNER JOIN chessboard ON (chessman.id = chessboard.id
 -- b) через UNION/INTERSECT/EXCEPT 
 
 -- 13) a
-SELECT board1.id_chessman FROM chessboard board1 LEFT JOIN chessboardAft board2 ON board1.id_chessman = board2.id_chessman WHERE
-        board1.x != board2.x OR board1.y != board2.y OR board2.id_chessman is NULL;
+SELECT board1.id_chessman FROM chessboard board1 LEFT 
+        JOIN chessboardAft board2 ON board1.id_chessman = board2.id_chessman 
+        WHERE board1.x != board2.x OR board1.y != board2.y OR board2.id_chessman is NULL;
         
 -- 13) b
 -- Скушанные и перемещенные фигуры.
-SELECT id_chessman FROM chessboard EXCEPT SELECT id_chessman FROM chessboardAft UNION SELECT chessboard.id_chessman FROM 
-        chessboard, chessboardAft WHERE chessboard.id_chessman = chessboardAft.id_chessman AND 
-        (chessboard.x != chessboardAft.x OR chessboard.y != chessboardAft.y);
+SELECT id_chessman FROM chessboard EXCEPT SELECT id_chessman FROM chessboardAft 
+        UNION SELECT chessboard.id_chessman FROM chessboard, chessboardAft 
+        WHERE chessboard.id_chessman = chessboardAft.id_chessman AND (chessboard.x != chessboardAft.x OR chessboard.y != chessboardAft.y);
         
 -- 14) Вывести id фигуры, если она стоит в «опасной близости» от черного короля? «опасной близостью» будем считать квадрат 5х5 с 
 -- королем в центре.
-SELECT board2.id_chessman FROM chessman figures, chessboard board1, chessboard board2 WHERE figures.type_of = 'king' AND figures.color = 'black'AND
+SELECT board2.id_chessman FROM chessman figures, chessboard board1, chessboard board2 
+        WHERE figures.type_of = 'king' AND figures.color = 'black'AND
         board1.id_chessman = figures.id AND board1.id_chessman != board2.id_chessman AND ABS(board2.x - board1.x) <= 2 AND 
         ABS(board2.y - board1.y) <= 2;
  
 -- 15) Найти фигуру, ближе всех стоящую к белому королю (расстояние считаем по метрике L1 – разница координат по X + разница координат по Y. 
-SELECT TOP(1) WITH TIES board2.id_chessman FROM chessman figures, chessboard board1, chessboard board2 WHERE figures.type_of = 'king' 
-        AND figures.color = 'white'AND board1.id_chessman = figures.id AND board1.id_chessman != board2.id_chessman 
+SELECT TOP(1) WITH TIES board2.id_chessman FROM chessman figures, chessboard board1, chessboard board2 
+        WHERE figures.type_of = 'king' AND figures.color = 'white'AND board1.id_chessman = figures.id AND 
+        board1.id_chessman != board2.id_chessman 
         ORDER BY (ABS(board2.x - board1.x) + ABS(board2.y - board1.y)) ASC;
 
 -- 16) Найти фигуры, которые может съесть ладья (Cid ладьи задан). Помните, что «своих» есть нельзя!
-SELECT board2.id_chessman FROM chessman figureRook, chessman figuresRest, chessboard board1, chessboard board2 WHERE board1.id_chessman = 1 
-        AND figureRook.id = 1 AND board2.id_chessman = figuresRest.id AND figureRook.color != figuresRest.color
-        AND board2.id_chessman != 1 AND 
-        ((board2.x - board1.x = 0) OR (board2.y - board1.y = 0));
+SELECT board2.id_chessman FROM chessman figureRook, chessman figuresRest, chessboard board1, chessboard board2 
+        WHERE board1.id_chessman = 1 AND figureRook.id = 1 AND board2.id_chessman = figuresRest.id AND figureRook.color != figuresRest.color
+        AND board2.id_chessman != 1 AND ((board2.x - board1.x = 0) OR (board2.y - board1.y = 0));
+        
+
+
+-- ПРОЦЕДУРА
+-- Процедура – «сделать ход». Если мы встали на клетку, где стояла фигура другого
+-- цвета, то «съесть» ее, если своего, то такой ход делать нельзя.
+CREATE PROCEDURE MakeMove(@x1 INT, @y1 INT, @x2 INT, @y2 INT) AS
+BEGIN
+    IF SELECT COUNT id_chessman FROM chessboard WHERE chessboard.x = x1 AND chessboard.y = y1
+        
+
+END
 
 
 
